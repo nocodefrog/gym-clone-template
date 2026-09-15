c64dcee72f5bd432968ab0a2ece7f01321fd9eb9
nocodefrog <nocodefrog806@gmail.com>
2026-09-16 01:39:48 +0530

    last update

--- data/location.ts
+++ data/location.ts
@@ -1,5 +1,5 @@
 export const location = {
   title: "Visit IronCore Fitness",
-  address: "123 Fitness Avenue, Your City, India",
+  address: "193 Fitness Avenue, Your City, India",
   mapUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d30163.940638957843!2d82.00428809601824!3d19.08603481286151!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a30126bc5ae6beb%3A0xcae4d4ac10952ba7!2sJagdalpur%2C%20Chhattisgarh%20494001!5e0!3m2!1sen!2sin!4v1789499908073!5m2!1sen!2sin",
 };
